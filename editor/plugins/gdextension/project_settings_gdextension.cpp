Izaaqk/Godot

Allow creating GDExtension plugins from inside the Godot editor

editor/plugins/gdextension/project_settings_gdextension.cpp
@@ -1,0 +1,137 @@
+/**************************************************************************/
+/*  project_settings_gdextension.cpp                                      */
+/**************************************************************************/
+/*                         This file is part of:                          */
+/*                             GODOT ENGINE                               */
+/*                        https://godotengine.org                         */
+/**************************************************************************/
+/* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
+/* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
+/*                                                                        */
+/* Permission is hereby granted, free of charge, to any person obtaining  */
+/* a copy of this software and associated documentation files (the        */
+/* "Software"), to deal in the Software without restriction, including    */
+/* without limitation the rights to use, copy, modify, merge, publish,    */
+/* distribute, sublicense, and/or sell copies of the Software, and to     */
+/* permit persons to whom the Software is furnished to do so, subject to  */
+/* the following conditions:                                              */
+/*                                                                        */
+/* The above copyright notice and this permission notice shall be         */
+/* included in all copies or substantial portions of the Software.        */
+/*                                                                        */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
+/**************************************************************************/
+
+#include "project_settings_gdextension.h"
+
+#include "cpp_scons/cpp_scons_gdext_creator.h"
+#include "gdextension_config_dialog.h"
+#include "gdextension_create_dialog.h"
+#include "gdextension_creator_plugin.h"
+
+#include "core/extension/gdextension_manager.h"
+#include "editor/editor_file_system.h"
+#include "editor/themes/editor_scale.h"
+#include "scene/gui/tree.h"
+
+class GDExtensionPluginCreatorBase;
+
+const int BUTTON_EDIT = 0;
+
+void ProjectSettingsGDExtension::_notification(int p_what) {
+	switch (p_what) {
+		case NOTIFICATION_WM_WINDOW_FOCUS_IN: {
+			_update_extension_tree();
+		} break;
+		case Node::NOTIFICATION_READY: {
+			extension_list->connect("button_clicked", callable_mp(this, &ProjectSettingsGDExtension::_cell_button_pressed));
+		} break;
+	}
+}
+
+void ProjectSettingsGDExtension::_cell_button_pressed(Object *p_item, int p_column, int p_id, MouseButton p_button) {
+	if (p_button != MouseButton::LEFT) {
+		return;
+	}
+	TreeItem *item = Object::cast_to<TreeItem>(p_item);
+	if (!item) {
+		return;
+	}
+	if (p_id == BUTTON_EDIT && p_column == COLUMN_EDIT) {
+		String path = item->get_metadata(COLUMN_PATH);
+		config_dialog->load_gdextension_config(path);
+		config_dialog->popup_centered(Size2(400, 300) * EDSCALE);
+	}
+}
+
+void ProjectSettingsGDExtension::_update_extension_tree() {
+	extension_list->clear();
+	TreeItem *root = extension_list->create_item();
+	Vector<String> extensions = GDExtensionManager::get_singleton()->get_loaded_extensions();
+	for (const String &extension : extensions) {
+		TreeItem *item = extension_list->create_item(root);
+		item->set_text(COLUMN_PATH, extension);
+		item->add_button(COLUMN_EDIT, get_editor_theme_icon(SNAME("Edit")), BUTTON_EDIT, false, TTR("Edit Extension"));
+		item->set_metadata(COLUMN_PATH, extension);
+	}
+}
+
+ProjectSettingsGDExtension::ProjectSettingsGDExtension() {
+	config_dialog = memnew(GDExtensionConfigDialog);
+	add_child(config_dialog);
+	create_dialog = memnew(GDExtensionCreateDialog);
+	add_child(create_dialog);
+	create_dialog->connect("gdextension_created", callable_mp(this, &ProjectSettingsGDExtension::_on_gdextension_created));
+
+	HBoxContainer *title_hb = memnew(HBoxContainer);
+	Label *label = memnew(Label(TTR("Installed GDExtensions:")));
+	label->set_theme_type_variation("HeaderSmall");
+	title_hb->add_child(label);
+	title_hb->add_spacer();
+	Button *create_plugin_button = memnew(Button(TTR("Create GDExtension")));
+	create_plugin_button->connect(SceneStringName(pressed), callable_mp(this, &ProjectSettingsGDExtension::_on_create_gdextension_pressed));
+	title_hb->add_child(create_plugin_button);
+	add_child(title_hb);
+
+	extension_list = memnew(Tree);
+	extension_list->set_v_size_flags(SIZE_EXPAND_FILL);
+	extension_list->set_hide_root(true);
+	extension_list->set_columns(COLUMN_MAX);
+	extension_list->set_column_titles_visible(true);
+	extension_list->set_column_title(COLUMN_PATH, TTR("Path"));
+	extension_list->set_column_title(COLUMN_EDIT, TTR("Edit"));
+	extension_list->set_column_title_alignment(COLUMN_PATH, HORIZONTAL_ALIGNMENT_LEFT);
+	extension_list->set_column_title_alignment(COLUMN_EDIT, HORIZONTAL_ALIGNMENT_LEFT);
+	extension_list->set_column_expand(COLUMN_PADDING_LEFT, false);
+	extension_list->set_column_expand(COLUMN_PATH, true);
+	extension_list->set_column_expand(COLUMN_EDIT, false);
+	extension_list->set_column_expand(COLUMN_PADDING_RIGHT, false);
+	extension_list->set_column_clip_content(COLUMN_PADDING_LEFT, true);
+	extension_list->set_column_clip_content(COLUMN_PATH, true);
+	extension_list->set_column_clip_content(COLUMN_EDIT, true);
+	extension_list->set_column_clip_content(COLUMN_PADDING_RIGHT, true);
+	extension_list->set_column_custom_minimum_width(COLUMN_PADDING_LEFT, 10 * EDSCALE);
+	extension_list->set_column_custom_minimum_width(COLUMN_PATH, 300 * EDSCALE);
+	extension_list->set_column_custom_minimum_width(COLUMN_EDIT, 40 * EDSCALE);
+	extension_list->set_column_custom_minimum_width(COLUMN_PADDING_RIGHT, 10 * EDSCALE);
+
+	add_child(extension_list);
+}
+
+void ProjectSettingsGDExtension::_on_create_gdextension_pressed() {
+	Vector<Ref<GDExtensionCreatorPlugin>> creators;
+	creators.append(memnew(CppSconsGDExtensionCreator));
+	create_dialog->load_plugin_creators(creators);
+	create_dialog->popup_centered(Size2(400, 300) * EDSCALE);
+}
+
+void ProjectSettingsGDExtension::_on_gdextension_created() {
+	EditorFileSystem::get_singleton()->scan();
+	_update_extension_tree();
+}

editor/plugins/gdextension/project_settings_gdextension.h
@@ -1,5 +1,5 @@
 /**************************************************************************/
-/*  plugin_config_dialog.h                                                */
+/*  project_settings_gdextension.h                                        */
 /**************************************************************************/
 /*                         This file is part of:                          */
 /*                             GODOT ENGINE                               */
@@ -28,63 +28,40 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
 /**************************************************************************/
 
-#ifndef PLUGIN_CONFIG_DIALOG_H
-#define PLUGIN_CONFIG_DIALOG_H
+#ifndef PROJECT_SETTINGS_GDEXTENSION_H
+#define PROJECT_SETTINGS_GDEXTENSION_H
 
-#include "scene/gui/check_box.h"
-#include "scene/gui/dialogs.h"
-#include "scene/gui/line_edit.h"
-#include "scene/gui/option_button.h"
-#include "scene/gui/panel_container.h"
-#include "scene/gui/text_edit.h"
-#include "scene/gui/texture_rect.h"
+#include "scene/gui/box_container.h"
 
-class ConfigFile;
-class EditorValidationPanel;
+class GDExtensionConfigDialog;
+class GDExtensionCreateDialog;
+class Tree;
 
-class PluginConfigDialog : public ConfirmationDialog {
-	GDCLASS(PluginConfigDialog, ConfirmationDialog);
+class ProjectSettingsGDExtension : public VBoxContainer {
+	GDCLASS(ProjectSettingsGDExtension, VBoxContainer);
 
 	enum {
-		MSG_ID_PLUGIN,
-		MSG_ID_SUBFOLDER,
-		MSG_ID_SCRIPT,
-		MSG_ID_ACTIVE,
+		COLUMN_PADDING_LEFT,
+		COLUMN_PATH,
+		COLUMN_EDIT,
+		COLUMN_PADDING_RIGHT,
+		COLUMN_MAX,
 	};
 
-	LineEdit *name_edit = nullptr;
-	LineEdit *subfolder_edit = nullptr;
-	TextEdit *desc_edit = nullptr;
-	LineEdit *author_edit = nullptr;
-	LineEdit *version_edit = nullptr;
-	OptionButton *script_option_edit = nullptr;
-	LineEdit *script_edit = nullptr;
-	CheckBox *active_edit = nullptr;
+	GDExtensionConfigDialog *config_dialog = nullptr;
+	GDExtensionCreateDialog *create_dialog = nullptr;
+	Tree *extension_list = nullptr;
 
-	LocalVector<Control *> plugin_edit_hidden_controls;
-
-	EditorValidationPanel *validation_panel = nullptr;
-
-	bool _edit_mode = false;
-
-	void _clear_fields();
-	void _on_confirmed();
-	void _on_canceled();
-	void _on_required_text_changed();
-	void _create_script_for_plugin(const String &p_plugin_path, Ref<ConfigFile> p_config_file, int p_script_lang_index);
-	String _get_subfolder();
-
-	static String _to_absolute_plugin_path(const String &p_plugin_name);
+	void _on_create_gdextension_pressed();
+	void _on_gdextension_created();
+	void _cell_button_pressed(Object *p_item, int p_column, int p_id, MouseButton p_button);
+	void _update_extension_tree();
 
 protected:
 	void _notification(int p_what);
-	static void _bind_methods();
 
 public:
-	void config(const String &p_config_path);
-
-	PluginConfigDialog();
-	~PluginConfigDialog();
+	ProjectSettingsGDExtension();
 };
 
-#endif // PLUGIN_CONFIG_DIALOG_H
+#endif // PROJECT_SETTINGS_GDEXTENSION_H

Reham-Ali/mygodot

editor/plugins/gdextension/project_settings_gdextension.cpp
@@ -1,0 +1,137 @@
+/**************************************************************************/
+/*  project_settings_gdextension.cpp                                      */
+/**************************************************************************/
+/*                         This file is part of:                          */
+/*                             GODOT ENGINE                               */
+/*                        https://godotengine.org                         */
+/**************************************************************************/
+/* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
+/* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
+/*                                                                        */
+/* Permission is hereby granted, free of charge, to any person obtaining  */
+/* a copy of this software and associated documentation files (the        */
+/* "Software"), to deal in the Software without restriction, including    */
+/* without limitation the rights to use, copy, modify, merge, publish,    */
+/* distribute, sublicense, and/or sell copies of the Software, and to     */
+/* permit persons to whom the Software is furnished to do so, subject to  */
+/* the following conditions:                                              */
+/*                                                                        */
+/* The above copyright notice and this permission notice shall be         */
+/* included in all copies or substantial portions of the Software.        */
+/*                                                                        */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
+/**************************************************************************/
+
+#include "project_settings_gdextension.h"
+
+#include "cpp_scons/cpp_scons_gdext_creator.h"
+#include "gdextension_create_dialog.h"
+#include "gdextension_creator_plugin.h"
+#include "gdextension_edit_dialog.h"
+
+#include "core/extension/gdextension_manager.h"
+#include "editor/editor_file_system.h"
+#include "editor/themes/editor_scale.h"
+#include "scene/gui/tree.h"
+
+class GDExtensionPluginCreatorBase;
+
+const int BUTTON_EDIT = 0;
+
+void ProjectSettingsGDExtension::_notification(int p_what) {
+	switch (p_what) {
+		case NOTIFICATION_WM_WINDOW_FOCUS_IN: {
+			_update_extension_tree();
+		} break;
+		case Node::NOTIFICATION_READY: {
+			extension_list->connect("button_clicked", callable_mp(this, &ProjectSettingsGDExtension::_cell_button_pressed));
+		} break;
+	}
+}
+
+void ProjectSettingsGDExtension::_cell_button_pressed(Object *p_item, int p_column, int p_id, MouseButton p_button) {
+	if (p_button != MouseButton::LEFT) {
+		return;
+	}
+	TreeItem *item = Object::cast_to<TreeItem>(p_item);
+	if (!item) {
+		return;
+	}
+	if (p_id == BUTTON_EDIT && p_column == COLUMN_EDIT) {
+		String path = item->get_metadata(COLUMN_PATH);
+		config_dialog->load_gdextension_config(path);
+		config_dialog->popup_centered(Size2(400, 300) * EDSCALE);
+	}
+}
+
+void ProjectSettingsGDExtension::_update_extension_tree() {
+	extension_list->clear();
+	TreeItem *root = extension_list->create_item();
+	Vector<String> extensions = GDExtensionManager::get_singleton()->get_loaded_extensions();
+	for (const String &extension : extensions) {
+		TreeItem *item = extension_list->create_item(root);
+		item->set_text(COLUMN_PATH, extension);
+		item->add_button(COLUMN_EDIT, get_editor_theme_icon(SNAME("Edit")), BUTTON_EDIT, false, TTR("Edit Extension"));
+		item->set_metadata(COLUMN_PATH, extension);
+	}
+}
+
+ProjectSettingsGDExtension::ProjectSettingsGDExtension() {
+	create_dialog = memnew(GDExtensionCreateDialog);
+	add_child(create_dialog);
+	create_dialog->connect("gdextension_created", callable_mp(this, &ProjectSettingsGDExtension::_on_gdextension_created));
+	config_dialog = memnew(GDExtensionEditDialog);
+	add_child(config_dialog);
+
+	HBoxContainer *title_hb = memnew(HBoxContainer);
+	Label *label = memnew(Label(TTR("Installed GDExtensions:")));
+	label->set_theme_type_variation("HeaderSmall");
+	title_hb->add_child(label);
+	title_hb->add_spacer();
+	Button *create_plugin_button = memnew(Button(TTR("Create GDExtension")));
+	create_plugin_button->connect(SceneStringName(pressed), callable_mp(this, &ProjectSettingsGDExtension::_on_create_gdextension_pressed));
+	title_hb->add_child(create_plugin_button);
+	add_child(title_hb);
+
+	extension_list = memnew(Tree);
+	extension_list->set_v_size_flags(SIZE_EXPAND_FILL);
+	extension_list->set_hide_root(true);
+	extension_list->set_columns(COLUMN_MAX);
+	extension_list->set_column_titles_visible(true);
+	extension_list->set_column_title(COLUMN_PATH, TTR("Path"));
+	extension_list->set_column_title(COLUMN_EDIT, TTR("Edit"));
+	extension_list->set_column_title_alignment(COLUMN_PATH, HORIZONTAL_ALIGNMENT_LEFT);
+	extension_list->set_column_title_alignment(COLUMN_EDIT, HORIZONTAL_ALIGNMENT_LEFT);
+	extension_list->set_column_expand(COLUMN_PADDING_LEFT, false);
+	extension_list->set_column_expand(COLUMN_PATH, true);
+	extension_list->set_column_expand(COLUMN_EDIT, false);
+	extension_list->set_column_expand(COLUMN_PADDING_RIGHT, false);
+	extension_list->set_column_clip_content(COLUMN_PADDING_LEFT, true);
+	extension_list->set_column_clip_content(COLUMN_PATH, true);
+	extension_list->set_column_clip_content(COLUMN_EDIT, true);
+	extension_list->set_column_clip_content(COLUMN_PADDING_RIGHT, true);
+	extension_list->set_column_custom_minimum_width(COLUMN_PADDING_LEFT, 10 * EDSCALE);
+	extension_list->set_column_custom_minimum_width(COLUMN_PATH, 300 * EDSCALE);
+	extension_list->set_column_custom_minimum_width(COLUMN_EDIT, 40 * EDSCALE);
+	extension_list->set_column_custom_minimum_width(COLUMN_PADDING_RIGHT, 10 * EDSCALE);
+
+	add_child(extension_list);
+}
+
+void ProjectSettingsGDExtension::_on_create_gdextension_pressed() {
+	Vector<Ref<GDExtensionCreatorPlugin>> creators;
+	creators.append(memnew(CppSconsGDExtensionCreator));
+	create_dialog->load_plugin_creators(creators);
+	create_dialog->popup_centered(Size2(400, 300) * EDSCALE);
+}
+
+void ProjectSettingsGDExtension::_on_gdextension_created() {
+	EditorFileSystem::get_singleton()->scan();
+	_update_extension_tree();
+}

editor/plugins/gdextension/project_settings_gdextension.h
@@ -1,5 +1,5 @@
 /**************************************************************************/
-/*  editor_validation_panel.h                                             */
+/*  project_settings_gdextension.h                                        */
 /**************************************************************************/
 /*                         This file is part of:                          */
 /*                             GODOT ENGINE                               */
@@ -28,62 +28,40 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
 /**************************************************************************/
 
-#ifndef EDITOR_VALIDATION_PANEL_H
-#define EDITOR_VALIDATION_PANEL_H
+#ifndef PROJECT_SETTINGS_GDEXTENSION_H
+#define PROJECT_SETTINGS_GDEXTENSION_H
 
-#include "scene/gui/panel_container.h"
+#include "scene/gui/box_container.h"
 
-class Button;
-class Label;
-class VBoxContainer;
+class GDExtensionCreateDialog;
+class GDExtensionEditDialog;
+class Tree;
 
-class EditorValidationPanel : public PanelContainer {
-	GDCLASS(EditorValidationPanel, PanelContainer);
+class ProjectSettingsGDExtension : public VBoxContainer {
+	GDCLASS(ProjectSettingsGDExtension, VBoxContainer);
 
-public:
-	enum MessageType {
-		MSG_OK,
-		MSG_WARNING,
-		MSG_ERROR,
-		MSG_INFO,
+	enum {
+		COLUMN_PADDING_LEFT,
+		COLUMN_PATH,
+		COLUMN_EDIT,
+		COLUMN_PADDING_RIGHT,
+		COLUMN_MAX,
 	};
 
-	static const int MSG_ID_DEFAULT = 0; // Avoids hard-coding ID in dialogs with single-line validation.
+	GDExtensionCreateDialog *create_dialog = nullptr;
+	GDExtensionEditDialog *config_dialog = nullptr;
+	Tree *extension_list = nullptr;
 
-private:
-	VBoxContainer *message_container = nullptr;
-
-	HashMap<int, String> valid_messages;
-	HashMap<int, Label *> labels;
-
-	bool valid = false;
-	bool pending_update = false;
-
-	struct ThemeCache {
-		Color valid_color;
-		Color warning_color;
-		Color error_color;
-	} theme_cache;
-
-	void _update();
-
-	Callable update_callback;
-	Button *accept_button = nullptr;
+	void _on_create_gdextension_pressed();
+	void _on_gdextension_created();
+	void _cell_button_pressed(Object *p_item, int p_column, int p_id, MouseButton p_button);
+	void _update_extension_tree();
 
 protected:
 	void _notification(int p_what);
 
 public:
-	void add_line(int p_id, const String &p_valid_message = "");
-	void set_accept_button(Button *p_button);
-	void set_update_callback(const Callable &p_callback);
-
-	void update();
-	void set_message(int p_id, const String &p_text, MessageType p_type, bool p_auto_prefix = true);
-	int get_message_count() const;
-	bool is_valid() const;
-
-	EditorValidationPanel();
+	ProjectSettingsGDExtension();
 };
 
-#endif // EDITOR_VALIDATION_PANEL_H
+#endif // PROJECT_SETTINGS_GDEXTENSION_H